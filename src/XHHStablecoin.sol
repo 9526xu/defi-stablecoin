d4dfe0fba25e7391ba5019f6c777e131d714e8da
xurujian <xurujian@bytedance.com>
2025-10-26 17:40:05 +0800

    feat: 添加XHHStablecoin稳定币合约实现
    实现基于ERC20的可铸造和销毁的稳定币合约
    包含金额和地址有效性检查
    仅合约所有者可执行铸造和销毁操作

--- src/XHHStablecoin.sol
+++ src/XHHStablecoin.sol
@@ -6,12 +6,12 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract XHStablecoin is ERC20, Ownable, ERC20Burnable {
+contract XHHStablecoin is ERC20, Ownable, ERC20Burnable {
     error Stablecoin_AmountMustBeGreaterThan0();
     error Stablecoin_AddressMustBeValid();
     error Stablecoin_AmountMustBeLessThanOrEqualToBalance();
 
-    constructor() ERC20("XHStablecoin", "XHC") Ownable(msg.sender) {}
+    constructor() ERC20("XHHStablecoin", "XHH") Ownable(msg.sender) {}
 
     modifier _checkAmountIsValid(uint256 amount) {
         if (amount <= 0) {
@@ -32,7 +32,7 @@ contract XHStablecoin is ERC20, Ownable, ERC20Burnable {
         if (amount > balanceOf(msg.sender)) {
             revert Stablecoin_AmountMustBeLessThanOrEqualToBalance();
         }
-        
+
         super.burn(amount);
     }
 }
