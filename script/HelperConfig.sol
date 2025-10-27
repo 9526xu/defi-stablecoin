94691df809213c01dff30ce7a9b43fe9e394e1ec
xurujian <xurujian@bytedance.com>
2025-10-27 20:46:06 +0800

    feat(配置): 添加HelperConfig合约用于部署模拟代币和价格喂价
    添加HelperConfig.sol合约，用于在不同网络环境下部署模拟代币(WETH, WBTC)和对应的价格喂价合约。该合约会根据当前链ID自动选择配置，目前主要支持Anvil本地测试网的配置。

---
+++ script/HelperConfig.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+// In your test file or a setup script
+import {MockV3Aggregator} from "@chainlink/contracts/src/v0.8/tests/MockV3Aggregator.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {Script} from "forge-std/Script.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+
+contract HelperConfig is Script {
+
+
+    struct NetworkConfig {
+        address[] collateralTokens;
+        address[] collateralTokenPriceFeeds;
+    }
+
+    NetworkConfig internal networkConfig;
+
+    constructor() {
+        if (block.chainid == 11_155_111) {
+            //  networkConfig = getSepoliaEthConfig();
+        } else {
+            networkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (networkConfig.collateralTokens.length != 0) {
+            return networkConfig;
+        }
+
+        vm.startBroadcast();
+        // Deploy a mock price feed for WETH
+        MockV3Aggregator wethPriceFeed = new MockV3Aggregator(8, 200000000000);
+        ERC20Mock wethMock = new ERC20Mock();
+
+        // Deploy a mock price feed for WBTC
+        MockV3Aggregator wbtcPriceFeed = new MockV3Aggregator(8, 500000000000);
+        ERC20Mock wbtcMock = new ERC20Mock();
+        vm.stopBroadcast();
+
+        address[] memory collateralTokens = new address[](2);
+        collateralTokens[0] = address(wethMock);
+        collateralTokens[1] = address(wbtcMock);
+
+        address[] memory collateralTokenPriceFeeds = new address[](2);
+        collateralTokenPriceFeeds[0] = address(wethPriceFeed);
+        collateralTokenPriceFeeds[1] = address(wbtcPriceFeed);
+
+        return NetworkConfig({collateralTokens: collateralTokens, collateralTokenPriceFeeds: collateralTokenPriceFeeds});
+    }
+}
